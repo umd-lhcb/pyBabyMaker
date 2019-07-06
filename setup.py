1e6633253d9b2b7b6a3956ed17aa81ef5abbc772
Yipeng Sun <syp@umd.edu>
2019-07-06 16:49:11 -0400

    Add cython-generated cpp file so that the package can be installed from github.

---
+++ setup.py
@@ -0,0 +1,41 @@
+# Author: Yipeng Sun <syp at umd dot edu>
+# License: BSD 2-clause
+# Last Change: Sat Jul 06, 2019 at 04:48 PM -0400
+
+from distutils.core import setup, Extension
+
+import subprocess
+
+
+###########
+# Helpers #
+###########
+
+def get_pipe_output(cmd):
+    cmd_splitted = cmd.split(' ')
+    proc = subprocess.Popen(cmd_splitted, stdout=subprocess.PIPE)
+    result = proc.stdout.read().decode('utf-8')
+    return result.strip('\n')
+
+
+########################
+# Compile instructions #
+########################
+
+root_libdir = get_pipe_output('root-config --libdir')
+root_incdir = get_pipe_output('root-config --incdir')
+
+cxx_flags = get_pipe_output('root-config --cflags').split()
+extra_flags = cxx_flags
+
+TupleDumpExtension = Extension(
+    name="TupleDump",
+    sources=["pyBabyMaker/io/dump.cpp"],
+    libraries=["RIO", "Tree"],
+    library_dirs=[root_libdir],
+    include_dirs=[root_incdir],
+    extra_compile_args=extra_flags,
+    language='c++',
+)
+
+setup(ext_modules=[TupleDumpExtension])

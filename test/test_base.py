3d56b7a32e72431966bb7190bc157e4384a69949
Yipeng Sun <syp@umd.edu>
2019-08-30 20:49:56 -0400

    Fully convert to pytest.

--- test/test_base.py
+++ test/test_base.py
@@ -2,9 +2,9 @@
 #
 # Author: Yipeng Sun <syp at umd dot edu>
 # License: BSD 2-clause
-# Last Change: Fri Aug 30, 2019 at 08:15 PM -0400
+# Last Change: Fri Aug 30, 2019 at 08:49 PM -0400
 
-import unittest
+import pytest
 
 from pyBabyMaker.base import UniqueList
 from pyBabyMaker.base import BaseCppGenerator
@@ -14,21 +14,28 @@ from pyBabyMaker.base import BaseCppGenerator
 # Data structure #
 ##################
 
-def test_UniqueList__init__():
+@pytest.fixture
+def default_UniqueList():
+    return UniqueList([1, 2, 3, 1])
+
+
+def test_UniqueList__init__normal():
     test_list = UniqueList([1, 2, 3, 4])
     assert test_list == [1, 2, 3, 4]
 
 
-class UniqueListTester(unittest.TestCase):
-    def test___init__(self):
-        self.assertTrue(
-            [1, 2, 3, 4],
-            UniqueList([1, 2, 3, 4]),
-        )
-        self.assertTrue(
-            [1, 2, 3],
-            UniqueList([1, 2, 3, 1]),
-        )
+def test_UniqueList__init__duplicate(default_UniqueList):
+    assert default_UniqueList == [1, 2, 3]
+
+
+def test_UniqueList_append_normal(default_UniqueList):
+    default_UniqueList.append(4)
+    assert default_UniqueList == [1, 2, 3, 4]
+
+
+def test_UniqueList_append_duplicate(default_UniqueList):
+    default_UniqueList.append(1)
+    assert default_UniqueList == [1, 2, 3]
 
 
 #######################
@@ -43,113 +50,88 @@ class SimpleCppGenerator(BaseCppGenerator):
         pass
 
 
-class SimpleCppGeneratorTester(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.cpp_generator = SimpleCppGenerator(
-            additional_system_headers=['iostream'],
-            additional_user_headers=['include/dummy.h']
-        )
+@pytest.fixture
+def default_SimpleCppGenerator():
+    return SimpleCppGenerator(
+        additional_system_headers=['iostream'],
+        additional_user_headers=['include/dummy.h']
+    )
 
-    #####################
-    # Initializer tests #
-    #####################
 
-    def test_system_headers(self):
-        self.assertEqual(
-            ['TFile.h', 'TTree.h', 'TTreeReader.h', 'TBranch.h', 'iostream'],
-            self.cpp_generator.system_headers
-        )
+# Headers ######################################################################
 
-    def test_user_headers(self):
-        self.assertEqual(
-            ['include/dummy.h'],
-            self.cpp_generator.user_headers
-        )
+def test_SimpleCppGenerator_custom_system_headers(default_SimpleCppGenerator):
+    assert default_SimpleCppGenerator.system_headers == \
+        ['TFile.h', 'TTree.h', 'TTreeReader.h', 'TBranch.h', 'iostream']
 
-    ###############################
-    # Chunk code generation tests #
-    ###############################
 
-    def test_gen_headers(self):
-        self.assertEqual(
-            '''#include <TFile.h>
+def test_SimpleCppGenerator_custom_user_headers(default_SimpleCppGenerator):
+    assert default_SimpleCppGenerator.user_headers == ['include/dummy.h']
+
+
+def test_SimpleCppGenerator_gen_headers(default_SimpleCppGenerator):
+    assert default_SimpleCppGenerator.gen_headers() == \
+        '''#include <TFile.h>
 #include <TTree.h>
 #include <TTreeReader.h>
 #include <TBranch.h>
 #include <iostream>
 
 #include "include/dummy.h"
-''',
-            self.cpp_generator.gen_headers()
-        )
+'''
 
-    def test_gen_headers_no_user(self):
-        cpp_generator = SimpleCppGenerator()
-        self.assertEqual(
-            '''#include <TFile.h>
+
+def test_SimpleCppGenerator_gen_headers_no_user():
+    cpp_generator = SimpleCppGenerator()
+    assert cpp_generator.gen_headers() == \
+        '''#include <TFile.h>
 #include <TTree.h>
 #include <TTreeReader.h>
 #include <TBranch.h>
 
-''',
-            cpp_generator.gen_headers()
-        )
+'''
 
-    ######################
-    # C++ snippets tests #
-    ######################
 
-    def test_cpp_header_system(self):
-        self.assertEqual(
-            '#include <iostream>\n',
-            self.cpp_generator.cpp_header('iostream')
-        )
+# C++ snippets #################################################################
 
-    def test_cpp_header_user(self):
-        self.assertEqual(
-            '#include "include/dummy.h"\n',
-            self.cpp_generator.cpp_header('include/dummy.h', False)
-        )
+def test_SimpleCppGenerator_cpp_header_system(default_SimpleCppGenerator):
+    assert default_SimpleCppGenerator.cpp_header('iostream') == \
+        '#include <iostream>\n'
 
-    def test_cpp_make_var(self):
-        self.assertEqual(
-            'pre_Variable1_Old_suf',
-            self.cpp_generator.cpp_make_var(
-                'Variable1/Old',
-                prefix='pre', suffix='suf', separator='_'
-            )
-        )
 
-    def test_cpp_main(self):
-        self.assertEqual(
-            '''
+def test_SimpleCppGenerator_cpp_header_user(default_SimpleCppGenerator):
+    assert default_SimpleCppGenerator.cpp_header('include/dummy.h', False) == \
+        '#include "include/dummy.h"\n'
+
+
+def test_SimpleCppGenerator_cpp_make_var(default_SimpleCppGenerator):
+    assert default_SimpleCppGenerator.cpp_make_var(
+        'Variable1/Old',
+        prefix='pre', suffix='suf', separator='_'
+    ) == 'pre_Variable1_Old_suf'
+
+
+def test_SimpleCppGenerator_cpp_main(default_SimpleCppGenerator):
+    assert default_SimpleCppGenerator.cpp_main('body') == \
+        '''
 int main(int, char** argv) {
   body
   return 0;
-}''',
-            self.cpp_generator.cpp_main('body')
-        )
-
-    def test_cpp_TTree(self):
-        self.assertEqual(
-            'TTree tree("tree", "tree");\n',
-            self.cpp_generator.cpp_TTree('tree', 'tree')
-        )
-
-    def test_cpp_TTreeReader(self):
-        self.assertEqual(
-            'TTreeReader reader("tree", input_file);\n',
-            self.cpp_generator.cpp_TTreeReader('reader', 'tree', 'input_file')
-        )
-
-    def test_cpp_TTreeReaderValue(self):
-        self.assertEqual(
-            'TTreeReaderValue<float> value(reader, "some_branch");\n',
-            self.cpp_generator.cpp_TTreeReaderValue(
-                'float', 'value', 'reader', 'some_branch')
-        )
+}'''
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_SimpleCppGenerator_cpp_TTree(default_SimpleCppGenerator):
+    assert default_SimpleCppGenerator.cpp_TTree('tree', 'tree') == \
+        'TTree tree("tree", "tree");\n'
+
+
+def test_SimpleCppGenerator_cpp_TTreeReader(default_SimpleCppGenerator):
+    assert default_SimpleCppGenerator.cpp_TTreeReader(
+        'reader', 'tree', 'input_file') == \
+        'TTreeReader reader("tree", input_file);\n'
+
+
+def test_SimpleCppGenerator_cpp_TTreeReaderValue(default_SimpleCppGenerator):
+    assert default_SimpleCppGenerator.cpp_TTreeReaderValue(
+        'float', 'value', 'reader', 'some_branch') == \
+        'TTreeReaderValue<float> value(reader, "some_branch");\n'

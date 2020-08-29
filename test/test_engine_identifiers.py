d4b3f53b934ce0b297698766dcb630a886231319
Yipeng Sun <syp@umd.edu>
2020-08-29 16:52:44 +0800

    WIP #6: Basic unit test to match full line comments.

--- test/test_engine_identifiers.py
+++ test/test_engine_identifiers.py
@@ -2,10 +2,17 @@
 #
 # Author: Yipeng Sun <syp at umd dot edu>
 # License: BSD 2-clause
-# Last Change: Fri Aug 28, 2020 at 07:15 PM +0800
+# Last Change: Sat Aug 29, 2020 at 04:45 PM +0800
 
 from pyBabyMaker.engine.identifiers import full_line_id
 
 
-def test_full_line_id():
+def test_full_line_id_no_match():
     assert full_line_id.search('int a') is None
+
+
+def test_full_line_id_match():
+    result = full_line_id.search('   // {% for x in data.y %} ')
+    assert result is not None
+    assert result.group(1) == '   '
+    assert result.group(2) == 'for x in data.y '

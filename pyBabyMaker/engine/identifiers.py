9f457bada068e97c2df8f19d4974e5f1d49740f0
Yipeng Sun <syp@umd.edu>
2020-08-30 03:33:05 +0800

    Update documentation.

--- pyBabyMaker/engine/identifiers.py
+++ pyBabyMaker/engine/identifiers.py
@@ -2,13 +2,32 @@
 #
 # Author: Yipeng Sun <syp at umd dot edu>
 # License: BSD 2-clause
-# Last Change: Sat Aug 29, 2020 at 05:52 PM +0800
+# Last Change: Sun Aug 30, 2020 at 03:32 AM +0800
+"""
+This module defines various identifiers to extract macros from C++ files.
+"""
 
 import re
 
 
 class Identifier(object):
+    """
+    Abstraction of an identifiers. This class provide wrappers to ``re``
+    objects.
+
+    These wrappers will provide saner return value, and strip heading/trailing
+    white spaces if configured.
+    """
     def __init__(self, pattern, groups, strip_policy):
+        """
+        Initialize identifier.
+
+        :param str pattern: pattern to be compiled into a ``re`` object.
+        :param int: Specify number of groups in the matching regex
+        :param list: A list of boolean, indicating if group(``idx+1``) should
+                     be stripped. ``idx`` is the index of the boolean in this
+                     list.
+        """
         self.regex = re.compile(pattern)
         self.groups = groups
         self.strip_policy = [False] + strip_policy
@@ -22,12 +41,22 @@ class Identifier(object):
                 self.macro_idx = i
 
     def search(self, string):
+        """
+        Wrapper to ``re.search``.
+        """
         return self.strip_whitespaces(self.regex.search(string))
 
     def match(self, string):
+        """
+        Wrapper to ``re.match``.
+        """
         return self.strip_whitespaces(self.regex.match(string))
 
     def strip_whitespaces(self, match):
+        """
+        Strip white spaces if regular expression has a match, and the strip
+        policy for a particular group is set to ``True``.
+        """
         if match is not None:
             return [match.group(i).strip() if self.strip_policy[i] else
                     match.group(i)

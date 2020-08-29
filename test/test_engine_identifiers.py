153a912964bc90023a389044b37ed280bbadfa58
Yipeng Sun <syp@umd.edu>
2020-08-29 17:07:18 +0800

    Update unit test.

--- test/test_engine_identifiers.py
+++ test/test_engine_identifiers.py
@@ -2,17 +2,17 @@
 #
 # Author: Yipeng Sun <syp at umd dot edu>
 # License: BSD 2-clause
-# Last Change: Sat Aug 29, 2020 at 04:45 PM +0800
+# Last Change: Sat Aug 29, 2020 at 05:07 PM +0800
 
 from pyBabyMaker.engine.identifiers import full_line_id
 
 
 def test_full_line_id_no_match():
-    assert full_line_id.search('int a') is None
+    assert not full_line_id.search('int a')
 
 
 def test_full_line_id_match():
     result = full_line_id.search('   // {% for x in data.y %} ')
-    assert result is not None
-    assert result.group(1) == '   '
-    assert result.group(2) == 'for x in data.y '
+    assert result
+    assert result[1] == '   '
+    assert result[2] == 'for x in data.y'

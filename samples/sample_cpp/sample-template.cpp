38b9164dbb0080b3a4cfcd54bdd8f62441aec03d
Yipeng Sun <syp@umd.edu>
2020-09-04 03:56:07 +0800

    Add a sample C++ template for testing.

---
+++ samples/sample_cpp/sample-template.cpp
@@ -0,0 +1,5 @@
+// System headers
+// {% join: (format_list: "#include <{}>", directive.system_headers), "\n" %}
+
+// User headers
+// {% join: (format_list: "#include \\"{}\\"", directive.user_headers), "\n" %}

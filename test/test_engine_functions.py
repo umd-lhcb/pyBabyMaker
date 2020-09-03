d0792a1dcd039e908659a943ac321058dd128f4d
Yipeng Sun <syp@umd.edu>
2020-09-04 02:09:39 +0800

    Add test case for variable dereference.

--- test/test_engine_functions.py
+++ test/test_engine_functions.py
@@ -2,7 +2,7 @@
 #
 # Author: Yipeng Sun <syp at umd dot edu>
 # License: BSD 2-clause
-# Last Change: Mon Aug 31, 2020 at 08:44 PM +0800
+# Last Change: Fri Sep 04, 2020 at 02:09 AM +0800
 
 from pyBabyMaker.engine.functions import macro_funcs
 
@@ -17,3 +17,10 @@ def test_func_join():
 
 def test_func_format():
     assert macro_funcs['format']('{}-{}-{}', 1, 2, 3) == '1-2-3'
+
+
+def test_func_deref_var():
+    expr = 'a > b_1 && b_1 < c'
+    vars_to_deref = ['a', 'b_1', 'c']
+    assert macro_funcs['deref_var'](expr, vars_to_deref) == \
+        '(*a) > (*b_1) && (*b_1) < (*c)'

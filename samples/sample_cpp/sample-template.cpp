87094eaa7c6bba78bc1bc833f5bf6b52c5bfb10c
Yipeng Sun <syp@umd.edu>
2020-09-04 04:58:19 +0800

    Update sample template.

--- samples/sample_cpp/sample-template.cpp
+++ samples/sample_cpp/sample-template.cpp
@@ -3,3 +3,17 @@
 
 // User headers
 // {% join: (format_list: "#include \"{}\"", directive.user_headers), "\n" %}
+
+// Generator for each output tree
+// {% for output_tree, config in directive.trees->items: %}
+void generator_/* {% output_tree %} */(TFile *input_file, TFile *output_file) {
+  TTreeReader reader("/* {% config.input_tree %} */", input_file);
+  TTree output(/* {% format: "\"{}\", \"{}\"", output_tree, output_tree %} */);
+
+  // Load needed branches from ntuple
+  // {% for var in config.input_branches %}
+  // {% format: "TTreeReaderValue<{}> {}(reader, \"{}\");", var.type, var.name, var.name  %}
+  // {% endfor %}
+}
+
+// {% endfor %}

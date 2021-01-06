4205ac948f303db5fc78d097968fe20b92ed2513
Yipeng Sun <syp@umd.edu>
2021-01-06 23:39:07 +0100

    Provide a method to replace resolved variables in an expression.

--- pyBabyMaker/var_resolver.py
+++ pyBabyMaker/var_resolver.py
@@ -2,7 +2,9 @@
 #
 # Author: Yipeng Sun <syp at umd dot edu>
 # License: BSD 2-clause
-# Last Change: Wed Jan 06, 2021 at 11:32 PM +0100
+# Last Change: Wed Jan 06, 2021 at 11:41 PM +0100
+
+import re
 
 from dataclasses import dataclass, field
 from typing import Dict
@@ -20,5 +22,19 @@ class Variable(object):
     deps: Dict[str, list(str)] = field(
         default=[],
         default_factory=lambda x: {x: [find_all_vars(i) for i in x]})
-    resolved: list(str) = []
+    resolved: Dict[str, str] = []
     idx: int = 0
+
+
+class VariableResolver(object):
+    def __init__(self, ):
+        pass
+
+    @staticmethod
+    def sub(expr, vars_to_replace):
+        """
+        Substitute variables in an expression with the resolved variable names.
+        """
+        for orig, resolved in vars_to_replace.items():
+            expr = re.sub(r'\b'+orig+r'\b', resolved, expr)
+        return expr

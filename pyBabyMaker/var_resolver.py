daea6273a522089c37f08b371b16603f2afaa624
Yipeng Sun <syp@umd.edu>
2021-01-06 23:32:13 +0100

    Cleaner definition of the 'Variable' container.

--- pyBabyMaker/var_resolver.py
+++ pyBabyMaker/var_resolver.py
@@ -2,9 +2,10 @@
 #
 # Author: Yipeng Sun <syp at umd dot edu>
 # License: BSD 2-clause
-# Last Change: Wed Jan 06, 2021 at 11:18 PM +0100
+# Last Change: Wed Jan 06, 2021 at 11:32 PM +0100
 
 from dataclasses import dataclass, field
+from typing import Dict
 
 from pyBabyMaker.boolean.utils import find_all_vars
 
@@ -16,6 +17,8 @@ class Variable(object):
     """
     type: str
     name: str
-    deps: list(str) = field(
+    deps: Dict[str, list(str)] = field(
         default=[],
-        default_factory=lambda x: [find_all_vars(i) for i in x])
+        default_factory=lambda x: {x: [find_all_vars(i) for i in x]})
+    resolved: list(str) = []
+    idx: int = 0

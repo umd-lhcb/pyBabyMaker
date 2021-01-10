ce3a86d06f10d0eb780c6bc028a59242dae854a5
Yipeng Sun <syp@umd.edu>
2021-01-10 04:46:12 +0100

    Another experimental variable loader.

--- pyBabyMaker/var_resolver.py
+++ pyBabyMaker/var_resolver.py
@@ -2,7 +2,7 @@
 #
 # Author: Yipeng Sun <syp at umd dot edu>
 # License: BSD 2-clause
-# Last Change: Fri Jan 08, 2021 at 04:48 AM +0100
+# Last Change: Sun Jan 10, 2021 at 04:49 AM +0100
 
 import re
 
@@ -62,12 +62,55 @@ class VariableResolver(object):
     def __init__(self, namespace, ordering):
         self.namespace = namespace
         self.ordering = ordering
-        self.resolved_vars = []
 
-    def resolve(self, scope, variables):
-        for name, var in variables.items():
-            # Always resolve name in its own scope first
-            pass
+        self._resolved_vars = []
 
-            # Now resolve in the namespace, with different scopes
-            # ...and we follow a defined ordering
+    def resolve_var(self, scope, var, ordering):
+        load_seq = []
+        num_of_scopes = len(ordering)
+        deps = list(var.deps.values())[var.idx]
+
+        for idx, other_scope in enumerate(ordering):
+            for dep_var_name in deps:
+                dep_var_name_resolved = other_scope+'_'+dep_var_name
+                if dep_var_name_resolved in self._resolved_vars:
+                    # If it's already loaded somewhere else, just use it
+                    var.resolved[dep_var_name] = dep_var_name_resolved
+                    break
+
+                if dep_var_name in self.namespace[other_scope]:
+                    dep_var = self.namespace[other_scope][dep_var_name]
+                    if scope == other_scope and dep_var_name == var.name:
+                        continue  # Don't do circular resolution
+
+                    if idx+1 == num_of_scopes:
+                        # We assume we can always load variables from the last
+                        # scope
+                        var.resolved[dep_var_name] = dep_var_name_resolved
+                        self._resolved_vars.append(dep_var_name_resolved)
+                        load_seq.append(self.format_resolved(
+                            other_scope, dep_var))
+
+                    else:
+                        dep_load_status, dep_load_seq = self.resolve_var(
+                            other_scope, dep_var, ordering[idx:])
+                        if dep_load_status:
+                            var.resolved[dep_var_name] = dep_var_name_resolved
+                            load_seq += dep_load_seq
+                        else:
+                            break  # No point continue if a dep can't load
+
+        if len(var.resolved) == len(deps):
+            self._resolved_vars.append(scope+'_'+var.name)
+            load_seq.append(self.format_resolved(scope, var))
+            return True, load_seq  # Resolution successful
+
+        # See if we tried all possible rvalues for this variable
+        if var.next():  # this variable has more rvalues, try resolve it again
+            return self.resolve_var(scope, var, ordering)
+
+        return False, load_seq  # Failed to load
+
+    @staticmethod
+    def format_resolved(scope, var):
+        return (scope, var)

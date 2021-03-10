338b20d2e53a977b40273f5711bfbdee34b12ac2
Yipeng Sun <syp@umd.edu>
2021-03-11 00:30:51 +0100

    WIP #10: Add more tests for ntuple dumper.

--- test/test_io.py
+++ test/test_io.py
@@ -2,17 +2,19 @@
 #
 # Author: Yipeng Sun <syp at umd dot edu>
 # License: BSD 2-clause
-# Last Change: Wed Sep 09, 2020 at 04:53 AM +0800
+# Last Change: Thu Mar 11, 2021 at 12:30 AM +0100
 
 import os
 import pytest
 import yaml
 
 from pyBabyMaker.io.NestedYAMLLoader import NestedYAMLLoader
+from pyBabyMaker.io.TupleDump import PyTupleDump
 
 PWD = os.path.dirname(os.path.realpath(__file__))
 PARDIR = os.path.join(PWD, os.pardir)
 SAMPLE_YAML = os.path.join(PARDIR, 'samples', 'sample-babymaker.yml')
+SAMPLE_NTP = os.path.join(PARDIR, 'samples', 'sample.root')
 
 
 @pytest.fixture
@@ -31,3 +33,10 @@ def test_NestedYAMLLoader_subfile_values(default_Loader):
     result = default_Loader
     assert result['output']['YetAnotherTuple']['drop'] == [
         'Y_OWNPV_COV_', 'Y_OWNPV_P.*']
+
+
+def test_PyTupleDump():
+    result = PyTupleDump(SAMPLE_NTP).dump()
+
+    assert result['TupleB0/DecayTree']['CaloPrsE'] == 'float'
+    assert result['TupleB0WSPi/DecayTree']['D0_ENDVERTEX_COV_'] == 'float[3][3]'

a5bc2c30a00972154121d887b5352b9f30a19dd4
Yipeng Sun <syp@umd.edu>
2021-06-20 01:37:22 +0200

    WIP #12: Update C++ template to handle friend trees.

--- pyBabyMaker/cpp_templates/babymaker.cpp
+++ pyBabyMaker/cpp_templates/babymaker.cpp
@@ -5,6 +5,9 @@
 #include <TTree.h>
 #include <TTreeReader.h>
 #include <TBranch.h>
+#include <TString.h>
+
+#include <vector>
 
 // System headers
 // {% join: (format_list: "#include <{}>", directive.system_headers), "\n" %}
@@ -12,11 +15,14 @@
 // User headers
 // {% join: (format_list: "#include \"{}\"", directive.user_headers), "\n" %}
 
-// Generator for each output tree
-// {% for output_tree, config in directive.trees->items: %}
-void generator_/* {% output_tree %} */(TFile *input_file, TFile *output_file) {
-  TTreeReader reader("/* {% config.input_tree %} */", input_file);
-  TTree output(/* {% format: "\"{}\", \"{}\"", output_tree, output_tree %} */);
+using namespace std;
+
+// Generator for each output tree: one tree per file
+// {% for tree_out, config in directive.trees->items: %}
+void generator_/* {% guard: tree_out %} */(TTree *input_tree, TString output_prefix) {
+  auto output_file = new TFile(output_prefix + /* {% quote: tree_out %} */ + ".root");
+  TTreeReader reader(input_tree);
+  TTree output("tree", "tree");
 
   // Load needed branches from ntuple
   // {% for var in config.input %}
@@ -25,25 +31,25 @@ void generator_/* {% output_tree %} */(TFile *input_file, TFile *output_file) {
 
   // Define output branches
   // {% for var in config.output %}
-  //   {% format: "{} {};", var.type, var.fname %}
+  //   {% declare: var.type, var.fname %}
   //   {% format: "output.Branch(\"{}\", &{});", var.name, var.fname %}
   // {% endfor %}
 
   // Define temporary variables
   // {% for var in config.tmp %}
-  //   {% format: "{} {};", var.type, var.fname %}
+  //   {% declare: var.type, var.fname %}
   // {% endfor %}
 
   while (reader.Next()) {
     // Define variables required by selection
     // {% for var in config.pre_sel_vars %}
-    //   {% format: "{} = {};", var.fname, (deref_var: var.rval, config.input_br) %}
+    //   {% assign: var.fname, (deref_var: var.rval, config.input_br) %}
     // {% endfor %}
 
     if (/* {% join: (deref_var_list: config.sel, config.input_br), " && " %} */) {
       // Assign values for each output branch in this loop
       // {% for var in config.post_sel_vars %}
-      //   {% format: "{} = {};", var.fname, (deref_var: var.rval, config.input_br) %}
+      //   {% assign: var.fname, (deref_var: var.rval, config.input_br) %}
       // {% endfor %}
 
       output.Fill();
@@ -51,22 +57,51 @@ void generator_/* {% output_tree %} */(TFile *input_file, TFile *output_file) {
   }
 
   output_file->Write();
+  delete output_file;
 }
 
 // {% endfor %}
 
 int main(int, char** argv) {
-  TFile *input_file = new TFile(argv[1], "read");
-  TFile *output_file = new TFile(argv[2], "recreate");
+  TString in_prefix  = TString(argv[1]) + "/";
+  TString out_prefix = TString(argv[2]);
 
-  // {% for output_tree in directive.trees->keys: %}
-  generator_/* {% output_tree %} */(input_file, output_file);
+  TFile *ntuple = new TFile(in_prefix + /* {% quote: directive.ntuple %} */);
+
+  vector<TFile*> friend_ntuples;
+  // {% for friend in directive.friends %}
+    friend_ntuples.push_back(new TFile(in_prefix + /* {% quote: friend %} */));
   // {% endfor %}
 
-  output_file->Close();
+  // Define input trees and container to store associated friend trees
+  // {% for tree in directive.input_trees %}
+  //   {% format: "auto tree_{} = static_cast<TTree*>(ntuple->Get(\"{}\"));", (guard: tree), tree %}
+  //   {% format: "vector<TTree*> friends_{};", (guard: tree) %}
+  // {% endfor %}
 
-  delete input_file;
-  delete output_file;
+  // Handle friend trees
+  TTree* tmp_tree;
+  // {% for tree in directive.input_trees %}
+  //   {% for idx, state in enum: directive.tree_relations[tree] %}
+  //     {% if state then %}
+  //       {% format: "tmp_tree = static_cast<TTree*>(friend_ntuples[{}]->Get(\"{}\"));", idx, tree %}
+           tmp_tree->BuildIndex("runNumber", "eventNumber");
+           tree_/* {% guard: tree %} */->AddFriend(tmp_tree);
+           friends_/* {% guard: tree %} */.push_back(tmp_tree);
+  //     {% endif %}
+  //   {% endfor %}
+  // {% endfor %}
+
+  // {% for tree_out, prop in directive.trees->items: %}
+  //   {% format: "generator_{}(tree_{}, out_prefix);", (guard: tree_out), (guard: prop.input_tree) %}
+  // {% endfor %}
+
+  // Cleanups
+  delete ntuple;
+  for (auto ntp : friend_ntuples) delete ntp;
+  // {% for tree in directive.input_trees %}
+    for (auto tree : friends_/* {% guard: tree %} */) delete tree;
+  // {% endfor %}
 
   return 0;
 }
